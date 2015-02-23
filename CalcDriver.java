c9634fb33c815091d97d4687204ccb90a3fb01b6
nathansamano <nsv9982@saintjoe.edu>
2015-02-22 18:59:55 -0600

    Initial commit

---
+++ CalcDriver.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+/*
+ * Nathan Samano
+ * October 30, 2013
+ * Project 2: Driver class for Calculation.java
+ */
+public class CalcDriver {
+
+	public static void main(String[] args) {
+		while (true) {
+			String userInput = JOptionPane.showInputDialog(null, "Enter a postfix expression.\nType 'exit' to exit the program.");		// string used to hold user input
+			if (userInput.equals("exit")) { // exit
+				System.exit(0);
+			}
+			Calculation expression = new Calculation(userInput);		// use user string and make a calculation with it
+		
+			JOptionPane.showMessageDialog(null, expression, "Answer", JOptionPane.INFORMATION_MESSAGE, null);
+		}
+	} 
+}
